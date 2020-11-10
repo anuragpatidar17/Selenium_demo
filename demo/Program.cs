fdf9a544e798b997aa8d39cf0391b66fe291269f
anuragpatidar17 <anurag.patidar@yahoo.in>
2020-11-10 12:29:02 +0530

    third

--- demo/Program.cs
+++ demo/Program.cs
@@ -1,35 +1,82 @@
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-namespace SeleniumTest
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;			
+using OpenQA.Selenium.Firefox;	
+using OpenQA.Selenium.Chrome;	
+using OpenQA.Selenium.IE;
+
+namespace SeleniumBingTests
 {
-    class Program
+  /// <summary>
+  /// Summary description for MySeleniumTests
+  /// </summary>
+  [TestClass]
+  public class MySeleniumTests
+  {
+    private TestContext testContextInstance;
+    private IWebDriver driver;
+    private string appURL;
+
+    public MySeleniumTests()
     {
-        static void Main(string[] args)
-        {
-            Console.Write("test case started ");
-            //create the reference for the browser  
-            IWebDriver driver = new ChromeDriver();
-            // navigate to URL  
-            driver.Navigate().GoToUrl("https://www.google.com/");
-            Thread.Sleep(2000);
-            // identify the Google search text box  
-            IWebElement ele = driver.FindElement(By.Name("q"));
-            ele.SendKeys("java");
-            Thread.Sleep(2000);
-            //identify the google search button  
-            IWebElement ele1 = driver.FindElement(By.Name("btnK"));
-            // click on the Google search button  
-            ele1.Click();
-            Thread.Sleep(3000);
-            //close the browser  
-            driver.Close();
-            Console.Write("test case ended ");
-        }
     }
+
+    [TestMethod]
+    [TestCategory("Chrome")]
+    public void TheBingSearchTest()
+    {
+      driver.Navigate().GoToUrl(appURL + "/");
+      driver.FindElement(By.Id("sb_form_q")).SendKeys("Azure Pipelines");
+      driver.FindElement(By.Id("sb_form_go")).Click();
+      driver.FindElement(By.XPath("//ol[@id='b_results']/li/h2/a/strong[3]")).Click();
+      Assert.IsTrue(driver.Title.Contains("Azure Pipelines"), "Verified title of the page");
+    }
+
+    /// <summary>
+    ///Gets or sets the test context which provides
+    ///information about and functionality for the current test run.
+    ///</summary>
+    public TestContext TestContext
+    {
+      get
+      {
+        return testContextInstance;
+      }
+      set
+      {
+        testContextInstance = value;
+      }
+    }
+
+    [TestInitialize()]
+    public void SetupTest()
+    {
+      appURL = "http://www.bing.com/";
+
+      string browser = "Chrome";
+      switch(browser)
+      {
+        case "Chrome":
+          driver = new ChromeDriver();
+          break;
+        case "Firefox":
+          driver = new FirefoxDriver();
+          break;
+        case "IE":
+          driver = new InternetExplorerDriver();
+          break;
+        default:
+          driver = new ChromeDriver();
+          break;
+      }
+
+    }
+
+    [TestCleanup()]
+    public void MyTestCleanup()
+    {
+      driver.Quit();
+    }
+  }
 }
