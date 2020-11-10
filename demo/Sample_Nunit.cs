fa23e11845ae6ea1a7d372b0229d531e3427d1e5
anuragpatidar17 <anurag.patidar@yahoo.in>
2020-11-10 13:46:44 +0530

    four

--- demo/Sample_Nunit.cs
+++ demo/Sample_Nunit.cs
@@ -1,10 +1,47 @@
-﻿using System;
-namespace demo
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace SeleniumTest
 {
-    public class Sample_Nunit
+    class Program
     {
-        public Sample_Nunit()
+        IWebDriver driver = new ChromeDriver();
+
+
+        [SetUp]
+        public void startBrowser()
         {
+            driver.Url = "http://www.google.co.in";
+            Thread.Sleep(2000);
         }
+
+        [Test]
+        public void test()
+        {
+            IWebElement ele = driver.FindElement(By.Name("q"));
+            ele.SendKeys("anurag patidar");
+            Thread.Sleep(2000);
+            //identify the google search button  
+            IWebElement ele1 = driver.FindElement(By.Name("btnK"));
+            // click on the Google search button  
+            ele1.Click();
+            Thread.Sleep(3000);
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+
     }
 }
